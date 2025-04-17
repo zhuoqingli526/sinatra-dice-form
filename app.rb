ab88ec9f442c12ff21d6b29159af7ec377acfd4b
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-17 21:27:38 +0000

    fisrt_part

--- app.rb
+++ app.rb
@@ -7,3 +7,16 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+  @num_sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
+  erb(:results)
+end
