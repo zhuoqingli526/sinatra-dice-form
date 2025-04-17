b6d442866f7b1c13320d0b04719fee62bc6bedf8
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-17 21:39:25 +0000

    homepage

--- app.rb
+++ app.rb
@@ -2,10 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
 
 get("/process_roll") do
